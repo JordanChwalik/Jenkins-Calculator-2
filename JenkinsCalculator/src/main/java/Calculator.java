5a18c74d67f0a126fbbb7f29cf2a67138057573d
Spectrej10 <chwalik.jordan@gmail.com>
2023-03-03 16:21:01 -0700

    Fibonacci working

--- JenkinsCalculator/src/main/java/Calculator.java
+++ JenkinsCalculator/src/main/java/Calculator.java
@@ -39,7 +39,16 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        return fibonacci[n - 1];
     }
 
 
