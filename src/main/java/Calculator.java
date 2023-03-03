4487d7dc033f02350f604cf4c85332d16e107a07
Spectrej10 <chwalik.jordan@gmail.com>
2023-03-03 16:08:46 -0700

    new repository

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Calculator {
+
+    Calculator(){
+
+    }
+
+    int add(int a , int b){
+        return a + b;
+    }
+
+    int subtract(int a , int b){
+        return a - b;
+    }
+
+    int multiply(int a , int b){
+        return a * b;
+    }
+
+    int divide(int a , int b){
+        return a / b;
+    }
+
+
+    /*
+    Returns the n'th number in the fibonacci sequence
+    https://en.wikipedia.org/wiki/Fibonacci_number
+    Example below
+    n = x
+    0 = 0
+    1 = 1
+    2 = 1
+    3 = 2
+    4 = 3
+    5 = 5
+    .
+    .
+    .
+    etc
+     */
+    int fibonacciNumberFinder(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        List fibonacci = new ArrayList<Integer>();
+        fibonacci.add(1);
+        fibonacci.add(1);
+        for (int i = 2; i < n; ++i) {
+            int fib1 = (int) fibonacci.get(i - 2);
+            int fib2 = (int) fibonacci.get(i - 1);
+            fibonacci.add(fib1 + fib2);
+        }
+        return (int) fibonacci.get(fibonacci.size() - 1);
+    }
+
+
+    /*
+    Returns binary value of the given int number
+    The binary number will be stored as a string
+    if int a = 0 then this method returns: 0
+    if int a = 10 then this method returns: 1010
+    if int a = 16 then this method returns: 10000
+     */
+    String intToBinaryNumber(int n){
+        return Integer.toBinaryString(n);
+    }
+
+    /*
+    Create a completely unique String identifier for a given string
+    Each createdID must contain the string n in its unaltered Form
+    if String n = "Jason"
+    then the created ID could be = Jasonklfgn3jknnvksdfm - Because it contains the unaltered String n and is unique
+
+    if you run this function twice with the same String input, it must return 2 unique String IDs
+     */
+    String createUniqueID(String n){
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder(n);
+        for (int i = 0; i < 10; ++i) {
+            stringBuilder.append((char)(random.nextInt(26) + 'a'));
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+}
