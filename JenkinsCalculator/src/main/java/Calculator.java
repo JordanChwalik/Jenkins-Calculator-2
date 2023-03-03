181bf6c905e6d1e3e35674b364c1560630a18b7d
Spectrej10 <chwalik.jordan@gmail.com>
2023-03-03 16:17:22 -0700

    restarted

--- JenkinsCalculator/src/main/java/Calculator.java
+++ JenkinsCalculator/src/main/java/Calculator.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 class Calculator {
 
@@ -42,18 +39,7 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (n == 2) return 1;
-        List fibonacci = new ArrayList<Integer>();
-        fibonacci.add(1);
-        fibonacci.add(1);
-        for (int i = 2; i < n; ++i) {
-            int fib1 = (int) fibonacci.get(i - 2);
-            int fib2 = (int) fibonacci.get(i - 1);
-            fibonacci.add(fib1 + fib2);
-        }
-        return (int) fibonacci.get(fibonacci.size() - 1);
+        return 0;
     }
 
 
@@ -65,7 +51,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return Integer.toBinaryString(n);
+        return null;
     }
 
     /*
@@ -77,13 +63,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-        Random random = new Random();
-        StringBuilder stringBuilder = new StringBuilder(n);
-        for (int i = 0; i < 10; ++i) {
-            stringBuilder.append((char)(random.nextInt(26) + 'a'));
-        }
 
-        return stringBuilder.toString();
+        return null;
     }
 
 
